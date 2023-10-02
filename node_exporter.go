3a6ca95ec2eb48e161a3d2cb4d7f1f467d5e673b
chaolihf <chaolihf@gmail.com>
2023-10-02 10:35:15 +0800

    调整应用为模块

--- node_exporter.go
+++ node_exporter.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package node_expoter_main
 
 import (
 	"fmt"
@@ -142,7 +142,7 @@ func (h *handler) innerHandler(filters ...string) (http.Handler, error) {
 	return handler, nil
 }
 
-func main() {
+func Main() {
 	var (
 		metricsPath = kingpin.Flag(
 			"web.telemetry-path",
